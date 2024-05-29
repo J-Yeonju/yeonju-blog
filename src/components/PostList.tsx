611a2cfab22b3b9dc093c11971cefe1ec9a0dc91
J-Yeonju <j.yeonju11.18@gmail.com>
2024-05-29 14:00:42 +0900

    feat: Firestore로 데이터 가져오기_게시글 리스트 작업

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,63 +1,89 @@
-import { useState } from "react";
-import { Link } from "react-router-dom"
+import AuthContext from "context/AuthContext";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "firebaseApp";
+import { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 interface PostListProps {
-    hasNavigation?: boolean;
+  hasNavigation?: boolean;
 }
 
 type TabType = "all" | "my";
 
-export default function PostList({ hasNavigation = true }: PostListProps) {
-    const [activeTab, setActiveTab] = useState<TabType>("all");
+interface PostProps {
+  id: string;
+  title: string;
+  email: string;
+  summary: string;
+  content: string;
+  createdAt: string;
+}
 
-    return  (
-        <>
-            {hasNavigation && (
-                <div className="post__navigation">
-                    <div
-                      role="presentation"
-                      onClick={() => setActiveTab("all")}
-                      className={activeTab === "all" ? "post__navigation--active" : ""}
-                    >
-                      전체
-                    </div>
-                    <div
-                      role="presentation"
-                      onClick={() => setActiveTab("my")}
-                      className={activeTab === "my" ? "post__navigation--active" : ""}
-                    >
-                      나의 글
-                    </div>
+export default function PostList({ hasNavigation = true }: PostListProps) {
+  const [activeTab, setActiveTab] = useState<TabType>("all");
+  const [posts, setPosts] = useState<PostProps[]>([]);
+  const { user } = useContext(AuthContext);
+
+  const getPosts = async () => {
+    const datas = await getDocs(collection(db, "posts"));
+
+    datas?.forEach((doc) => {
+      const dataObj = { ...doc.data(), id: doc.id };
+      setPosts((prev) => [...prev, dataObj as PostProps]);
+    });
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  return (
+    <>
+      {hasNavigation && (
+        <div className="post__navigation">
+          <div
+            role="presentation"
+            onClick={() => setActiveTab("all")}
+            className={activeTab === "all" ? "post__navigation--active" : ""}
+          >
+            전체
+          </div>
+          <div
+            role="presentation"
+            onClick={() => setActiveTab("my")}
+            className={activeTab === "my" ? "post__navigation--active" : ""}
+          >
+            나의 글
+          </div>
+        </div>
+      )}
+      <div className="post__list">
+        {posts?.length > 0 ? (
+          posts?.map((post, index) => (
+            <div key={post?.id} className="post__box">
+              <Link to={`/posts/${post?.id}`}>
+                <div className="post__profile-box">
+                  <div className="post__profile" />
+                  <div className="post__author-name">{post?.email}</div>
+                  <div className="post__date">{post?.createdAt}</div>
                 </div>
-            )}
-            <div className="post__list">
-                {[...Array(10)].map((e, index) => (
-                    <div key={index} className="post__box">
-                        <Link to={`/posts/${index}`}>
-                            <div className="post__profile-box">
-                                <div className="post__profile" />
-                                <div className="post__author-name">연주</div>
-                                <div className="post__date">2024.05.09 목요일</div>
-                            </div>
-                            <div className="post__title">게시글 {index}</div>
-                            <div className="post__text">
-                            Happiness in this world, when it comes, comes incidentally. Make it the object of pursuit, and it leads us a wild-goose chase, and is never attained.
-                            All sorts of persons, and every individual, has a place to fill in the world, and is important in some respects, whether he chooses to be so or not.
-                            Selfishness is one of the qualities apt to inspire love. This might be thought out at great length.
-                            To see a young couple loving each other is no wonder: but to see an old couple loving each other is the best sight of all.
-                            Dare and the world always yield. If it beats you sometimes, dare it again and again and it will succumb.
-                            One is unworthy of being tolerated if he can't forgive others.
-                            No other misfortune can be compared with the loss of time.
-                            The word tomorrow was invented for indecisive people and for children.
-                            </div>
-                            <div className="post__utils-box">
-                                <div className="post__delete">삭제</div>
-                                <div className="post__edit">수정</div>
-                            </div>
-                        </Link>
-                    </div>
-                ))}
+                <div className="post__title">{post?.title}</div>
+                <div className="post__text">{post?.content}</div>
+              </Link>
+              {post?.email === user?.email && (
+                <div className="post__utils-box">
+                  <div className="post__delete">삭제</div>
+                  <Link to={`/posts/edit/${post?.id}`} className="post__edit">
+                    수정
+                  </Link>
+                </div>
+              )}
             </div>
-        </>
-    )
+          ))
+        ) : (
+          <div className="post__no-post">게시글이 없습니다.</div>
+        )}
+      </div>
+    </>
+  );
 }
